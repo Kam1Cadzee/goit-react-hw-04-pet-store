c65feb1e6d1ad69e9aec17c5e27bad3b8af4fbcc
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-23 01:27:42 +0300

    Updates

---
+++ precache-manifest.a07e1b486e71deff5808f4e5cdee6964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a9e2d78229934106326f2db3dba406",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "b0d6c0c3c8f75910f2a0",
+    "url": "/goit-react-hw-04-pet-store/static/css/AboutPage.edd0ad92.chunk.css"
+  },
+  {
+    "revision": "6ba2c5367317e3d6b0db",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.c5ec19e8.chunk.css"
+  },
+  {
+    "revision": "4995c3b6cfd4ca324364",
+    "url": "/goit-react-hw-04-pet-store/static/js/3.15ddf204.chunk.js"
+  },
+  {
+    "revision": "bfe2960301a4a6b3983f",
+    "url": "/goit-react-hw-04-pet-store/static/js/4.eb47a066.chunk.js"
+  },
+  {
+    "revision": "b0d6c0c3c8f75910f2a0",
+    "url": "/goit-react-hw-04-pet-store/static/js/AboutPage.098ba22c.chunk.js"
+  },
+  {
+    "revision": "6ba2c5367317e3d6b0db",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.6eb775ce.chunk.js"
+  },
+  {
+    "revision": "224dc20669b9e57354d8",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.2cc2a3ed.js"
+  }
+]);
