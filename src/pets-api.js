32a44dfa821ab0287c5654e78a5900430e96e9bc
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-24 20:21:59 +0300

    добавлен ajax запрос

--- src/pets-api.js
+++ src/pets-api.js
@@ -1,17 +1,16 @@
 import * as axios from 'axios';
-import data from './server/db.json';
-
-axios.defaults.baseURL = 'http://localhost:4040';
+import db from './server/db.json';
 
 export const getPets = () => {
-  /* return axios.get('/pets').then(response => {
-    return response.data;
-  }); */
-  return Promise.resolve(data.pets);
+  const url =
+    'https://pixabay.com/api/?key=12509636-53caf59f5bb94c5e817c60f6e&image_type=photo&orientation=horizontal&q=dogs&per_page=9';
+  return axios(url).then(data => {
+    return data.data.hits.map((item, i) => {
+      db.pets[i].image = item.webformatURL;
+      return db.pets[i];
+    });
+  });
 };
 export const getPet = id => {
-  /* return axios.get(`/pets/${id}`).then(response => {
-    return response.data;
-  }); */
-  return Promise.resolve(data.pets.find(pet => pet.id === id));
+  return Promise.resolve(db.pets.find(pet => pet.id === id));
 };
