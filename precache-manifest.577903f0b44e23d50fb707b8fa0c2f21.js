2fc2bc73cf144592522861bb5ba47f6838736f00
Kam1Cadzee <39504960+Kam1Cadzee@users.noreply.github.com>
2019-05-28 01:23:53 +0300

    Updates

---
+++ precache-manifest.577903f0b44e23d50fb707b8fa0c2f21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17138e4a20917246336306adcdd500a",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "25acbe69f7e322461391",
+    "url": "/goit-react-hw-04-pet-store/static/css/AboutPage.96713399.chunk.css"
+  },
+  {
+    "revision": "8251ff30bcab85aa8771",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.e79e449a.chunk.css"
+  },
+  {
+    "revision": "f493f5f52ee916c55b32",
+    "url": "/goit-react-hw-04-pet-store/static/js/3.54d8fc37.chunk.js"
+  },
+  {
+    "revision": "57b3265f2aa2bfef4f00",
+    "url": "/goit-react-hw-04-pet-store/static/js/4.78fd5bdd.chunk.js"
+  },
+  {
+    "revision": "25acbe69f7e322461391",
+    "url": "/goit-react-hw-04-pet-store/static/js/AboutPage.bc960e8a.chunk.js"
+  },
+  {
+    "revision": "8251ff30bcab85aa8771",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.60039b96.chunk.js"
+  },
+  {
+    "revision": "021744c2a8d72b0b57cf",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.cf206ae2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-pet-store/precache-manifest.3c7de3fcbbe1dd3493c5e8d3661af430.js"
+  "/goit-react-hw-04-pet-store/precache-manifest.577903f0b44e23d50fb707b8fa0c2f21.js"
 );
 
 self.addEventListener('message', (event) => {
